f607131bfa89284272bcc5d017a2d566fcd09864
zentag <38821230+zentag@users.noreply.github.com>
2025-05-16 12:23:34 -0400

    what the heck

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
+use rusqlite::{Connection, Result};
+
+#[derive(Debug)]
+struct Todo {
+    id: i32,
+    text: String,
+    done: bool,
+}
+const PATH: &str = "~/.todo/todos.db";
+fn main() -> Result<()> {
+    let conn = Connection::open(PATH)?;
+    conn.execute(
+        "CREATE TABLE todo (
+            id   INTEGER PRIMARY KEY,
+            name TEXT NOT NULL,
+            done BOOLEAN NOT NULL,
+        )",
+        (), // empty list of parameters.
+    )?;
     println!("Hello, world!");
+    return Ok(())
 }
