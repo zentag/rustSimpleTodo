d4434e0b35e8e33414e95a7dbc174b38284412ff
zentag <38821230+zentag@users.noreply.github.com>
2025-06-09 11:00:36 -0400

    try adding rust flake

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,25 @@
 use::std::io;
+use rusqlite::{Connection, Result};
 
-fn main() {
+#[derive(Debug)]
+struct Todo {
+    id: i32,
+    content: String,
+    done: bool,
+}
+fn main() -> std::result::Result<(), rusqlite::Error> {
+    println!("welcome to your todo list");
+    let conn = Connection::open("./db.db3")?;
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS todos (
+            id   INTEGER PRIMARY KEY,
+            content TEXT NOT NULL,
+            done INTEGER,
+        )",
+        (), // empty list of parameters.
+    )?;
     let mut todo = String::new();
-    io::stdin().read_line(&mut todo).expect("Couldn't read stdin");
+    io::stdin().read_line(&mut todo).expect("couldn't read stdin");
     println!("back to start");
+    Ok(())
 }
