c8d971e052b87f7c8d8e716989511f1d2da4b42f
zentag <38821230+zentag@users.noreply.github.com>
2025-06-09 11:25:04 -0400

    can readout todos

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ struct Todo {
     content: String,
     done: bool,
 }
-fn main() -> std::result::Result<(), rusqlite::Error> {
+fn main() -> Result<()> {
     println!("welcome to your todo list");
     let conn = Connection::open("./db.db3")?;
     conn.execute(
@@ -15,9 +15,24 @@ fn main() -> std::result::Result<(), rusqlite::Error> {
             id   INTEGER PRIMARY KEY,
             content TEXT NOT NULL,
             done INTEGER
-        )",
+        );",
         (), // empty list of parameters.
     )?;
+    conn.execute(" INSERT INTO todos (id,content ,done) VALUES( 123151,'relax' ,1);", ())?;
+
+    let mut stmt = conn.prepare("SELECT id, content, done FROM todos")?;
+    let todo_iter = stmt.query_map([], |row| {
+        Ok(Todo {
+            id: row.get(0)?,
+            content: row.get(1)?,
+            done: row.get(2)?,
+        })
+    })?;
+    for todo in todo_iter {
+        let todo = todo.unwrap();
+        println!("- [] {}", todo.content);
+        println!("{}", todo.done)
+    }
     let mut todo = String::new();
     io::stdin().read_line(&mut todo).expect("couldn't read stdin");
     println!("back to start");
