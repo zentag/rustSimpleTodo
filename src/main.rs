940f01713d5f73ba4560091805e7256df9bc42ec
zentag <38821230+zentag@users.noreply.github.com>
2025-06-08 20:11:43 -0400

    go all the way back to simple

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,3 @@
-use rusqlite::{Connection, Result};
-
-#[derive(Debug)]
-struct Todo {
-    id: i32,
-    text: String,
-    done: bool,
-}
-const PATH: &str = "~/.todo/todos.db";
-fn main() -> Result<()> {
-    let conn = Connection::open(PATH)?;
-    conn.execute(
-        "CREATE TABLE todo (
-            id   INTEGER PRIMARY KEY,
-            name TEXT NOT NULL,
-            done BOOLEAN NOT NULL,
-        )",
-        (), // empty list of parameters.
-    )?;
-    println!("Hello, world!");
-    return Ok(())
+fn main() {
+    println!("back to start");
 }
